5659c3f0db9561f0e92b9e31817e28af66bd1437
Zev Eisenberg <zev@zeveisenberg.com>
2016-01-31 19:23:46 -0500

    Always start UI tests in portrait orientation.

--- Padiddle/PadiddleUITests/PadiddleUITests.swift
+++ Padiddle/PadiddleUITests/PadiddleUITests.swift
@@ -20,6 +20,7 @@ class PadiddleUITests: XCTestCase {
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
 
+        XCUIDevice.sharedDevice().orientation = .Portrait
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
