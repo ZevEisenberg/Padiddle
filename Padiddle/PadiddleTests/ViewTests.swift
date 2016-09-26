a202741d1a99433f3d9ede0ab8eff6768068ee0e
Zev Eisenberg <zev@zeveisenberg.com>
2016-09-25 22:19:13 -0400

    Fix tests.

--- Padiddle/PadiddleTests/ViewTests.swift
+++ Padiddle/PadiddleTests/ViewTests.swift
@@ -11,10 +11,10 @@ import XCTest
 class ViewTests: XCTestCase {
 
     func testAccessibilityIdentifiers() {
-        let view = UIView("foobar")
+        let view = UIView(axId: "foobar")
         XCTAssertEqual(view.accessibilityIdentifier, "foobar")
 
-        let button = UIButton(type: .system, "foobar")
+        let button = UIButton(type: .system, axId: "foobar")
         XCTAssertEqual(button.accessibilityIdentifier, "foobar")
     }
 
