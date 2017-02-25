cca08d2ff30b9befec7e263bd2e621ce51bf695d
Zev Eisenberg <zev@zeveisenberg.com>
2017-02-25 17:35:06 -0500

    Update pods.

--- Padiddle/Constants/L10n.swift
+++ Padiddle/Constants/L10n.swift
@@ -83,7 +83,7 @@ extension L10n: CustomStringConvertible {
   }
 
   private static func tr(key: String, _ args: CVarArg...) -> String {
-    let format = NSLocalizedString(key, comment: "")
+    let format = NSLocalizedString(key, bundle: Bundle(for: BundleToken.self), comment: "")
     return String(format: format, locale: Locale.current, arguments: args)
   }
 }
@@ -91,3 +91,5 @@ extension L10n: CustomStringConvertible {
 func tr(_ key: L10n) -> String {
   return key.string
 }
+
+private final class BundleToken {}
