5dad5940255056e7a565e2fdc78828fb600ad0a9
Zev Eisenberg <zev@zeveisenberg.com>
2020-04-17 19:04:42 -0400

    Line wrap.

--- Padiddle/scripts/prettify_status_bar.sh
+++ Padiddle/scripts/prettify_status_bar.sh
@@ -17,5 +17,14 @@ if [ $(version "${TARGET_DEVICE_OS_VERSION}") -ge $(version "13") ]; then
     zeroWidthSpace='\xE2\x80\x8B' # UTF-8 encoding of u+200B in Bash (different format from zsh)
     timeString="$(date -r 1568122860 +%FT%T%z)" # 9:41 AM, with the correct date for iPads
 
-    xcrun simctl status_bar "${TARGET_DEVICE_IDENTIFIER}" override --time "${timeString}" --dataNetwork wifi --wifiMode active --wifiBars 3 --cellularMode active --operatorName $(echo -e $zeroWidthSpace) --cellularBars 4 --batteryState discharging --batteryLevel 100
+    xcrun simctl status_bar "${TARGET_DEVICE_IDENTIFIER}" override \
+         --time "${timeString}" \
+         --dataNetwork wifi \
+         --wifiMode active \
+         --wifiBars 3 \
+         --cellularMode active \
+         --operatorName $(echo -e $zeroWidthSpace) \
+         --cellularBars 4 \
+         --batteryState discharging \
+         --batteryLevel 100
 fi
